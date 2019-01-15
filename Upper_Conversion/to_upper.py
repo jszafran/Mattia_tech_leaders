68e9730733a1b4c20da3e925a6437924a54ea5f4
jszafran <jszafran.pv@gmail.com>
2019-01-15 18:25:42 +0100

    My solution for upper conversion.

---
+++ Upper_Conversion/to_upper.py
@@ -0,0 +1,35 @@
+def convert_to_upper(s):
+    if not isinstance(s, str):
+        raise "Function accepts only string argument."
+
+    d = {
+        "a": "A",
+        "b": "B",
+        "c": "C",
+        "d": "D",
+        "e": "E",
+        "f": "F",
+        "g": "G",
+        "h": "H",
+        "i": "I",
+        "j": "J",
+        "k": "K",
+        "l": "L",
+        "m": "M",
+        "n": "N",
+        "o": "O",
+        "p": "P",
+        "q": "Q",
+        "r": "R",
+        "s": "S",
+        "t": "T",
+        "u": "U",
+        "v": "V",
+        "w": "W",
+        "x": "X",
+        "y": "Y",
+        "z": "Z"
+    }
+
+    return "".join([d[c] if c in d else c for c in s])
+    

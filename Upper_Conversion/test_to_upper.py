94f5a85796c765e4d95e10154ce1ef843b8bfc55
jszafran <jszafran.pv@gmail.com>
2019-01-16 20:41:12 +0100

    Inserted blank lines between methods.

--- Upper_Conversion/test_to_upper.py
+++ Upper_Conversion/test_to_upper.py
@@ -1,13 +1,15 @@
 import pytest
 import to_upper as up
 
+
 def test_convert_to_upper():
     assert up.convert_to_upper('Just Testing') == 'JUST TESTING'
 
-
+    
 def test_convert_to_upper_with_punctuation():
     assert up.convert_to_upper('test, with.punctuation') == 'TEST, WITH.PUNCTUATION'
 
+    
 def test_raise_typerror_exception():
     with pytest.raises(TypeError):
         up.convert_to_upper(1000)
